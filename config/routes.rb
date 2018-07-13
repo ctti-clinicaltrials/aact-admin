b2795709e82d7650ded70269cd9a6756385482c8
tibbs001 <sheri.tibbs@duke.edu>
2018-07-13 16:53:53 -0400

    aact-691:  start to flesh out the tech documentation

--- config/routes.rb
+++ config/routes.rb
@@ -6,38 +6,39 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
-  get "/learn_more"           => "pages#learn_more"
-  get "/schema"               => "pages#schema"
   get "/data_dictionary"      => "dictionary#show"
-  get "/credentials"          => "credentials#show"
   get "/activities"           => "database_activity#show"
   get 'dictionary/show'
 
   get "/connect"              => "pages#connect"
-  get "/pgadmin"              => "pages#pgadmin"
-  get "/r"                    => "pages#r"
-  get "/sas"                  => "pages#sas"
-  get "/psql"                 => "pages#psql"
-  get "/technical_documentation"    => "pages#technical_documentation"
-  get "/release_notes"        => "pages#release_notes"
-  get "/update_policy"        => "pages#update_policy"
-
-  get "/points_to_consider"   => "pages#points_to_consider"
-  get "/install_postgres"     => "postgres_documentation#install_postgres"
-  get "/faq"                  => "faq#index"
-
   get "/download"             => "pages#download"
-  get "/snapshots"            => "pages#snapshots"
+  get "/learn_more"           => "pages#learn_more"
   get "/pipe_files"           => "pages#pipe_files"
   get "/pipe_files_with_r"    => "pages#pipe_files_with_r"
   get "/pipe_files_with_sas"  => "pages#pipe_files_with_sas"
-  get "/snapshot_archive"     => "pages#snapshot_archive"
-
+  get "/pgadmin"              => "pages#pgadmin"
+  get "/points_to_consider"   => "pages#points_to_consider"
+  get "/psql"                 => "pages#psql"
+  get "/r"                    => "pages#r"
+  get "/release_notes"        => "pages#release_notes"
   get "/sanity_check_report"  => "pages#sanity_check", as: :sanity_check
+  get "/sas"                  => "pages#sas"
+  get "/schema"               => "pages#schema"
+  get "/snapshots"            => "pages#snapshots"
+  get "/snapshot_archive"     => "pages#snapshot_archive"
+  get "/technical_documentation"    => "pages#technical_documentation"
+  get "/update_policy"        => "pages#update_policy"
+
+  get "/install_postgres"     => "postgres_documentation#install_postgres"
+  get "/credentials"          => "credentials#show"
+  get "/faq"                  => "faq#home"
+  get "/admin_run_loads"      => "faq#admin_run_loads"
+  get "/admin_remove_user"    => "faq#admin_remove_user"
+  get "/tech_access_servers"  => "faq#access_servers"
+
 
   resources :definitions
   resources :users
-  resources :projects
   resources :use_cases
   resources :use_case_attachments
 end
