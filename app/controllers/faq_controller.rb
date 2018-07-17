4e321f171d5ae61fdbe2cc264346931e7992e645
tibbs001 <sheri.tibbs@duke.edu>
2018-07-17 14:46:16 -0400

    aact-691:  Prevent non-admins from accessing FAQ

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -1,3 +1,16 @@
 class FaqController < ApplicationController
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    render plain: "Sorry - This page is only accessible to administrators." if !current_user_is_an_admin?
+  end
+
+  def current_user_is_an_admin?
+    return false if !current_user
+    col=ENV['AACT_ADMIN_USERNAMES'].split(',')
+    col.include? current_user.username
+  end
 
 end
