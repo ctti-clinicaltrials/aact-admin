3d6f812ecaae2f92e83b4ab7d07cc0f3597772f5
tibbs001 <sheri.tibbs@duke.edu>
2018-11-12 17:52:03 -0500

    aact-821:  Project classes are now saved in the Proj module (not Public)

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,7 +1,7 @@
 class AttachmentsController < ApplicationController
 
   def show
-    @attachment=Public::Attachment.find(params['id'])
+    @attachment=Proj::Attachment.find(params['id'])
     send_data(@attachment.file_contents,
              type: @attachment.content_type,
              filename: @attachment.file_name,
--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,7 +1,7 @@
 class DatasetsController < ApplicationController
 
   def show
-    @dataset=Public::Dataset.find(params['id'])
+    @dataset=Proj::Dataset.find(params['id'])
     send_data(@dataset.file_contents,
              type: @dataset.content_type,
              filename: @dataset.file_name,
