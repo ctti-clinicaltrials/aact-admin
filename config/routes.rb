dbfa94189e8b6b2e4ada5d4959351d611bbcd627
tibbs001 <sheri.tibbs@duke.edu>
2019-04-02 15:49:30 -0400

    aact-935:  Use the project’s schema_name rather than ID in the url that displays the individual project page.  (The ID will change, but the schema_name should stay the same, so we can share this link with other systems such as SOAR.)

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Rails.application.routes.draw do
 
   resources :definitions
   resources :users
-  resources :projects
+  resources :projects, param: :schema_name
   resources :release_notes
   resources :datasets
   resources :attachments
