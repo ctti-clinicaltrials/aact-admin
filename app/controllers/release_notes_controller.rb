78cbe9b78b02a2d2505b0f62d98404daedc0d391
tibbs001 <sheri.tibbs@duke.edu>
2019-05-02 15:40:26 -0400

    add controller and model code from aact-952-upgrade-gems-security-vulnerability

--- app/controllers/release_notes_controller.rb
+++ app/controllers/release_notes_controller.rb
@@ -3,6 +3,7 @@ class ReleaseNotesController < ApplicationController
   def index
     fpm=Util::FilePresentationManager.new
     @schema_diagram=fpm.schema_diagram
+    @nested_criteria_example=fpm.nested_criteria_example
   end
 
 end
