45c55595d4994f6b58d6c00f8cc95f94e767f403
tibbs001 <sheri.tibbs@duke.edu>
2019-06-25 09:03:23 -0400

    aact-1039:  user db activity info not getting collected. Since upgrade to postgres 11.1, the log files have a different code to indicate user event.  Change string too grep for.

--- app/models/db_user_activity.rb
+++ app/models/db_user_activity.rb
@@ -3,7 +3,7 @@ class DbUserActivity < ActiveRecord::Base
 
   def self.import_from_logs
     file_name = Util::FileManager.new.todays_db_activity_file
-    system("ssh #{AACT::Application::AACT_DB_SUPER_USERNAME}@#{AACT::Application::AACT_DB_HOSTNAME} sudo grep STATEMENT #{AACT::Application::AACT_STATIC_FILE_DIR}/logs/postgresql-*.log | grep -i select | cut -d ' ' -f 5 | sort | uniq -c > #{file_name}")
+    system("ssh #{AACT::Application::AACT_DB_SUPER_USERNAME}@#{AACT::Application::AACT_DB_HOSTNAME} sudo grep EDT #{AACT::Application::AACT_STATIC_FILE_DIR}/logs/postgresql-*.log | grep -i select | cut -d ' ' -f 5 | sort | uniq -c > #{file_name}")
     populate_from_file(file_name,'weekly')
     update_user_records
   end
