8cf60c13ce95873e573a5388b01fe666b4e87583
Crystal Williams-Brown <crystal@codethedream.org>
2021-04-07 17:14:20 -0400

    working on method for granting privileges

--- app/models/util/db_manager.rb
+++ app/models/util/db_manager.rb
@@ -53,6 +53,52 @@ module Util
       AACT::Application::AACT_PUBLIC_DATABASE_NAME
     end
 
+    def revoke_db_privs
+      log "  db_manager: set connection limit so only db owner can login..."
+      public_con.execute("ALTER DATABASE #{db_name} CONNECTION LIMIT 0;")
+      public_alt_con.execute("ALTER DATABASE #{alt_db_name} CONNECTION LIMIT 0;")
+    end
+
+    def grant_db_privs
+      public_con.execute("ALTER DATABASE #{db_name} CONNECTION LIMIT 200;")
+      public_con.execute("GRANT USAGE ON SCHEMA ctgov TO read_only;")
+      public_con.execute("GRANT SELECT ON ALL TABLES IN SCHEMA CTGOV TO READ_ONLY;")
+      public_alt_con.execute("ALTER DATABASE #{alt_db_name} CONNECTION LIMIT 200;")
+      public_alt_con.execute("GRANT USAGE ON SCHEMA ctgov TO read_only;")
+      public_alt_con.execute("GRANT SELECT ON ALL TABLES IN SCHEMA CTGOV TO READ_ONLY;")
+    end
+
+    def alt_db_name
+      AACT::Application::AACT_ALT_PUBLIC_DATABASE_NAME
+    end
+
+    def db_name
+      AACT::Application::AACT_PUBLIC_DATABASE_NAME
+    end
+    def public_con
+      return @public_con if @public_con and @public_con.active?
+      PublicBase.establish_connection(public_db_url)
+      @public_con = PublicBase.connection
+      @public_con.schema_search_path='ctgov'
+      return @public_con
+    end
+
+    def public_alt_con
+      return @public_alt_con if @public_alt_con and @public_alt_con.active?
+      PublicBase.establish_connection(alt_db_url)
+      @public_alt_con = PublicBase.connection
+      @public_alt_con.schema_search_path='ctgov'
+      return @public_alt_con
+    end
+
+    # def con
+    #   return @con if @con and @con.active?
+    #   ActiveRecord::Base.establish_connection(back_db_url)
+    #   @con = ActiveRecord::Base.connection
+    #   @con.schema_search_path=@search_path
+    #   return @con
+    # end
+
   end
 
 end
