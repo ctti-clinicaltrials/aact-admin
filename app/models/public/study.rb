9e79656fe5a6501845d79617202ea625a112ec46
kholtzman5610 <kassidy.holtzman@outlook.com>
2021-08-31 15:24:38 -0400

    Removed TaggedTerms

--- app/models/public/study.rb
+++ app/models/public/study.rb
@@ -22,7 +22,6 @@ module Public
     self.primary_key = 'nct_id'
 
     def self.ids_tagged(tag)
-      terms=Public::TaggedTerm.terms_for_tag(tag).pluck(:term).uniq
       ids=Public::BrowseCondition.ids_for_terms(terms) + Public::BrowseIntervention.ids_for_terms(terms)
       return ids
     end
