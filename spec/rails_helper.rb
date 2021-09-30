fc2ace5b770a9046ade0478a3d4ced2968a754a4
Crystal Williams-Brown <crystal@codethedream.org>
2021-09-30 18:31:28 -0400

    set use_transactional_fixtures to true

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,7 +23,7 @@ RSpec.configure do |config|
   config.include Features, type: :feature
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   config.include FormHelpers, :type => :feature
   config.include Devise::Test::ControllerHelpers, type: :controller
