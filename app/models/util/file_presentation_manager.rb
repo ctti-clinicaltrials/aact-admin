685db81fe5b183f563f856162209b4b6a2c40256
Asiya Bykova <asya.bykova@gmail.com>
2021-11-10 10:21:57 -0500

    Added lost changes from dev

--- app/models/util/file_presentation_manager.rb
+++ app/models/util/file_presentation_manager.rb
@@ -12,15 +12,21 @@ module Util
       "https://prsinfo.clinicaltrials.gov/results_definitions.html"
     end
 
-    def monthly_snapshot_files
+    def monthly_snapshot_files(schema='')
+      return Util::FileManager.new.files_in('beta_static_db_copies','monthly') if schema == 'beta'
+
       Util::FileManager.new.files_in('static_db_copies','monthly')
     end
 
-    def daily_snapshot_files
+    def daily_snapshot_files(schema='')
+      return Util::FileManager.new.files_in('beta_static_db_copies','daily') if schema == 'beta'
+
       Util::FileManager.new.files_in('static_db_copies','daily')
     end
 
-    def monthly_flat_files
+    def monthly_flat_files(schema='')
+      return Util::FileManager.new.files_in('beta_exported_files','monthly') if schema == 'beta'
+
       Util::FileManager.new.files_in('exported_files','monthly')
     end
 
