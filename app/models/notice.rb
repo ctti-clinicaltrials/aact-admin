5b6ea795ebf40d949829f4319f6894674f7f24af
Ramiro Rodriguez <ramiro.rodriguez@gmail.com>
2021-11-12 12:39:33 -0500

    add more feedback to sending email

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -15,7 +15,12 @@ class Notice < ActiveRecord::Base
   after_create :send_notice
 
   def send_notice
-    User.all.each {|user| NoticeMailer.notice_to_mail(user.email, self).deliver_now}
+    users = User.all.order(email: :asc)
+    remaining = users.count
+    users.each do |user|
+      puts "#{remaining} #{user.email}"
+      NoticeMailer.notice_to_mail(user.email, self).deliver_now
+    end
     self.emails_sent_at = Time.now
     self.save
   end
