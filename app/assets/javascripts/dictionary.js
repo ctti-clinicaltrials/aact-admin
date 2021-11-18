8505325c4515b47fa500716855ec93a2b2dfd1ba
Asiya Bykova <asya.bykova@gmail.com>
2021-11-18 12:10:46 -0500

    Changes to PR

--- app/assets/javascripts/dictionary.js
+++ app/assets/javascripts/dictionary.js
@@ -15,9 +15,19 @@ $(function() {
         pageSize: 40,
         controller: {
             loadData: function(filter) {
+
+              var schema=$("#jsGrid").data('schema')
+              let url;
+              if (schema == "archive"){
+                url = "archive";
+              } else if (schema == "ctgov"){
+                url = "ctgov";
+              } else {
+                url = "beta";
+              }
                 return $.ajax({
                     type: "GET",
-                    url: '/definitions?schema=ctgov',
+                    url: `/definitions?schema=${url}`,
                     data: filter
                 });
             }
@@ -34,72 +44,4 @@ $(function() {
             { name: 'source',             type: "text", width: 250 },
         ]
     });
-    $("#jsBetaGrid").jsGrid({
-          height: "160vh",
-          width: "100%",
-          filtering: true,
-          inserting: false,
-          editing: false,
-          sorting: true,
-          paging: true,
-          autoload: true,
-          noDataContent: "...Loading...",
-          searchButtonTooltip: "Search",
-          clearFilterButtonTooltip: "Clear filter",
-          pageSize: 40,
-          controller: {
-              loadData: function(filter) {
-                  return $.ajax({
-                      type: "GET",
-                      url: "/definitions?schema=beta",
-                      data: filter
-                  });
-              }
-          },
-          fields: [
-              { type: 'control', deleteButton: false, editButton: false },
-              { name: 'nlm doc',            width: 38, align: 'center' },
-              { name: 'db schema',          type: "text", width: 70 },
-              { name: 'table',              type: "text", width: 150 },
-              { name: 'column',             type: "text", width: 180 },
-              { name: 'data type',          type: "text", width: 70 },
-              { name: 'CTTI note',          type: "text", width: 350 },
-              { name: 'enumerations',       type: "text", width: 250 },
-              { name: 'source',             type: "text", width: 250 },
-          ]
-      });
-      $("#jsArchiveGrid").jsGrid({
-            height: "160vh",
-            width: "100%",
-            filtering: true,
-            inserting: false,
-            editing: false,
-            sorting: true,
-            paging: true,
-            autoload: true,
-            noDataContent: "...Loading...",
-            searchButtonTooltip: "Search",
-            clearFilterButtonTooltip: "Clear filter",
-            pageSize: 40,
-            controller: {
-                loadData: function(filter) {
-                    return $.ajax({
-                        type: "GET",
-                        url: "/definitions?schema=archive",
-                        data: filter
-                    });
-                }
-            },
-            fields: [
-                { type: 'control', deleteButton: false, editButton: false },
-                { name: 'nlm doc',            width: 38, align: 'center' },
-                { name: 'db schema',          type: "text", width: 70 },
-                { name: 'table',              type: "text", width: 150 },
-                { name: 'column',             type: "text", width: 180 },
-                { name: 'data type',          type: "text", width: 70 },
-                { name: 'CTTI note',          type: "text", width: 350 },
-                { name: 'enumerations',       type: "text", width: 250 },
-                { name: 'source',             type: "text", width: 250 },
-            ]
-        });
 });
