96b00bbfc736c07b7e5ec840becf550b8b6d0d03
Javier Jimenez <javierjimenez@Accounts-MacBook-Pro.local>
2021-12-11 13:49:06 -0800

    merged and resolved the conflict in Gemfile.lock, structure.sql, and factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,13 @@ FactoryBot.define do
     body { "MyText" }
   end
 
+  factory :notice do
+    body { "MyString" }
+    user_id { 1 }
+    title { "MyString" }
+    send_emails { false }
+    emails_sent_at { "2021-10-27 18:07:43" }
+    visible { false }
+  end  
+
 end
