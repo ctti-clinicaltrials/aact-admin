d97f4a4893c4b451370cc680c409498fff68338d
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2022-02-22 15:41:16 -0800

    Updated summary controller and aact action/page for the database summary.

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -2,11 +2,10 @@ class SummaryController < ApplicationController
 
   def aact
     @updates = Public::Study.connection.execute("SELECT updated_at::date AS last_updated,
-                                        count(*) AS number_of_studies
-                                        from studies
-                                        group by updated_at::date
-                                        order by updated_at::date asc"
-                                        )
+                                                count(*) AS number_of_studies
+                                                from studies
+                                                group by updated_at::date
+                                                order by updated_at::date asc")
   end
 
 end
