188b22abd27fc2dd0253df69b9d2a50f88348a29
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2022-08-17 14:27:49 -0700

    Updated Query controller and requests spec test files to reflect changing Query from a POST to a GET request.

--- spec/controllers/query_controller_spec.rb
+++ spec/controllers/query_controller_spec.rb
@@ -2,16 +2,9 @@ require 'rails_helper'
 
 RSpec.describe QueryController, type: :controller do
 
-  describe "GET #submit" do
+  describe "GET #index" do
     it "returns http success" do
-      get :submit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "POST #index" do
-    it "returns http success" do
-      post :index, query: 'SELECT nct_id, study_type, brief_title, enrollment, has_dmc, completion_date, updated_at FROM studies LIMIT 20'
+      get :index, query: 'SELECT nct_id, study_type, brief_title, enrollment, has_dmc, completion_date, updated_at FROM studies LIMIT 20'
       expect(response).to have_http_status(:success)
     end
   end
