3c4df7cf20a144b3348c77a34c9741fa8b985d60
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2022-08-23 15:44:45 -0700

    Generated the csv in ruby by modifying the QueryController index action. Deleted the index.csv.erb view file.

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -10,11 +10,20 @@ class QueryController < ApplicationController
         @error = [e.message]
       end
     end
+
     respond_to do |format|
       format.csv  do
         response.headers['Content-Type'] = 'text/csv'
         response.headers['Content-Disposition'] = 'attachment; filename=query.csv'
-        render template: 'query/index.csv.erb'
+        # get headers from the keys of query results to generate first line of csv file
+        headers = @results.first.map { |key,value| key }
+        csv = CSV.generate_line headers
+        # get rows from the values of the keys of query results to generate rest of lines of csv file
+        @results.each do |result|
+          rows = result.each.map { |key,value| value }  
+          csv << CSV.generate_line(rows).html_safe
+        end
+        render plain: csv
       end
       format.html { render template: 'query/index.html.erb' }
     end
