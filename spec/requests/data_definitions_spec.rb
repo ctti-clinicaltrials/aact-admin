b48ceceac85e7f01f59caf03127eedbab97fec9e
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2022-09-02 08:26:02 -0700

    Removed comments in Data Definitions requests RSpec file.

--- spec/requests/data_definitions_spec.rb
+++ spec/requests/data_definitions_spec.rb
@@ -44,19 +44,6 @@ RSpec.describe "Data Definitions", type: :request do
       end
     end
 
-    # describe "POST /data_definitions/ with invalid data" do
-    #   it "does not save a new Data Definition or redirects if invalid db_section" do
-    #     data_def_attributes = { data_definition: {db_section: '', table_name: 'AACT', data_type: 'integer', ctti_note: 'Date NLM made study available via their API'} }
-    #     expect { post data_definitions_path, data_def_attributes }.to_not change(DataDefinition, :count)
-    #     expect(response).to render_template(:new)
-    #   end
-    #   it "does not save a new Data Definition or redirects if invalid ctti_note" do
-    #     data_def_attributes = { data_definition: {db_section: 'title', table_name: 'AACT', data_type: 'boolean', ctti_note: ''} }
-    #     expect { post data_definitions_path, data_def_attributes }.to_not change(DataDefinition, :count)
-    #     expect(response).to render_template(:new)
-    #   end
-    # end
-
     describe "PUT /data_definitions/ with valid data" do
       it "updates a Data Definition and redirects to the show page if valid attribute" do
         data_def = FactoryBot.create(:data_definition)
@@ -66,23 +53,6 @@ RSpec.describe "Data Definitions", type: :request do
         expect(response).to redirect_to data_definitions_path
       end
     end
-
-    # describe "PUT /data_definitions/ with invalid data" do
-    #   it "does not update the Data Definition or redirects if invalid table_name" do
-    #     data_def = FactoryBot.create(:data_definition)
-    #     put data_definition_path(data_def.id), data_definition: {table_name: ''}
-    #     data_def.reload
-    #     expect(data_def.table_name).not_to eq('')
-    #     expect(response).to render_template(:edit)
-    #   end
-    #   it "does not update the Data Definition or redirects if invalid data_type" do
-    #     data_def = FactoryBot.create(:data_definition)
-    #     put data_definition_path(data_def.id), data_definition: {data_type: ''}
-    #     data_def.reload
-    #     expect(data_def.data_type).not_to eq('')
-    #     expect(response).to render_template(:edit)
-    #   end
-    # end
 
     describe "DELETE /data_definitions " do
       it "deletes a Data Definition from Data Definitions" do
