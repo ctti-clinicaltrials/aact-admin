f0471691c96ab44cd263d566847fd40da667ea94
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2022-10-05 10:51:46 -0700

    Fixed bug so that the Verifier show and index page does not crash if a load event does not have a verifier. Created Verifiers requests tests Spec file. Updated factories Spec file to be able to run requests tests.

---
+++ spec/requests/verifiers_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Verifiers", type: :request do
+
+    describe "GET /verifiers" do
+      it "renders the Verifiers index page" do
+        get verifiers_path
+        expect(response).to render_template(:index)
+      end
+    end
+
+    describe "GET /verifiers" do
+      it "renders the Verifier show page" do
+        verify = FactoryBot.create(:verifier)
+        get verifier_path(id: verify.id)
+        expect(response).to render_template(:show)
+      end
+      it "redirects to the Verifiers index page if Verifier ID is invalid" do
+        get verifier_path(id: 5000) # an ID that doesn't exist
+        expect(response).to redirect_to verifiers_path
+      end
+    end
+
+end    
