3e0d5f47f0ce35e1b98e55bcc272e4e4e4084a59
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2022-10-24 11:38:37 -0700

    Modified QueriesController and view _form.

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -27,7 +27,7 @@ class QueriesController < ApplicationController
     def catch_not_found(e)
       Rails.logger.debug("We had a not found exception.")
       flash.alert = e.to_s
-      redirect_to queries_path
+      # redirect_to queries_path
     end
 
 end
