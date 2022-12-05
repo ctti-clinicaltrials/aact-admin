77a676c7772a3a112841ae45491b1b413adead80
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2022-12-05 15:25:48 -0800

    Updated SavedQueriesController Rspec Controller and Request test files. The Request test file has some issues and will be investigated further.

--- spec/controllers/saved_queries_controller_spec.rb
+++ spec/controllers/saved_queries_controller_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe SavedQueriesController, type: :controller do
+  before do
+    @user = User.create(email: 'UserEmail@email.com', first_name: 'Firstname', last_name: 'Lastname', username: 'user123', password: '1234567', db_activity: nil, last_db_activity: nil, admin: false)
+    @user.confirm
+    sign_in(@user)
+    @user2 = User.create(email: 'User2Email@email.com', first_name: 'Firstname', last_name: 'Lastname2', username: 'user12345', password: '1234567', db_activity: nil, last_db_activity: nil, admin: false)
+    @user2.confirm
+  end
+
+  after do
+    # delete all Queries associated with each User before deleting each User
+    @user.saved_queries.each do |query|
+      query.destroy
+    end  
+    @user.destroy
+    @user2.saved_queries.each do |query|
+      query.destroy
+    end  
+    @user2.destroy
+  end
+  
   describe "GET #new" do
     it "returns http success" do
       get :new
@@ -11,14 +31,10 @@ RSpec.describe SavedQueriesController, type: :controller do
 
   describe "POST #create" do
     it "returns http found" do
-      @user = User.create(email: 'Jackie.Robinson42@email.com', first_name: 'Jackie', last_name: 'Robinson', username: 'jackierobinson42', password: 'Dodgers', db_activity: nil, last_db_activity: nil, admin: false)
-      @user.confirm
-      sign_in(@user)
       saved_q = FactoryBot.attributes_for(:saved_query)
       post :create, saved_query: saved_q
-      expect(response).to redirect_to saved_query_path
+      expect(response).to redirect_to saved_query_path(id: SavedQuery.last.id)
       expect(response).to have_http_status(:found)
-      @user.destroy
     end
   end
 
@@ -40,15 +56,17 @@ RSpec.describe SavedQueriesController, type: :controller do
   end
 
   describe "GET #edit" do
-    it "returns http success" do
-      @user = User.create(email: 'Sandy.Koufax32@email.com', first_name: 'Sandy', last_name: 'Koufax', username: 'sandykoufax32', password: 'Dodgers', db_activity: nil, last_db_activity: nil, admin: false)
-      @user.confirm
-      sign_in(@user)
-      saved_q = FactoryBot.create(:saved_query)
+    it "returns http success if the current logged-in User is the User that created the Query" do
+      saved_q = FactoryBot.create(:saved_query, user_id: @user.id)
       get :edit, id: saved_q.id
       expect(response).to render_template(:edit)
       expect(response).to have_http_status(:success)
-      @user.destroy
+    end
+    it "redirects to the index page if the current logged-in User is NOT the User that created the Query" do
+      saved_q = FactoryBot.create(:saved_query, user_id: @user2.id)
+      get :edit, id: saved_q.id
+      expect(response).to redirect_to(saved_queries_path)
+      expect(response).to have_http_status(:found)
     end
   end
 
@@ -56,22 +74,23 @@ RSpec.describe SavedQueriesController, type: :controller do
     it "returns http found" do
       saved_q = FactoryBot.create(:saved_query)
       put :update, id: saved_q.id, saved_query: { "public"=>"true" }
-      expect(response).to redirect_to (@saved_query)
+      expect(response).to redirect_to saved_query_path
       expect(response).to have_http_status(:found)
     end
   end
 
   describe "DELETE #destroy" do
-    it "returns http success" do
-      @user = User.create(email: 'Clayton.Kershaw22@email.com', first_name: 'Clayton', last_name: 'Kershaw', username: 'claytonkershaw22', password: 'Dodgers', db_activity: nil, last_db_activity: nil, admin: false)
-      @user.confirm
-      sign_in(@user)
-      saved_q = FactoryBot.create(:saved_query)
+    it "returns http found if the current logged-in User is the User that created the Query" do
+      saved_q = FactoryBot.create(:saved_query, user_id: @user.id)
       delete :destroy, id: saved_q.id
-      expect(response).to redirect_to (:index)
+      expect(response).to redirect_to saved_queries_path
+      expect(response).to have_http_status(:found)
+    end
+    it "renders the show page if the current logged-in User is NOT the User that created the Query" do
+      saved_q = FactoryBot.create(:saved_query, user_id: @user2.id)
+      delete :destroy, id: saved_q.id
+      expect(response).to render_template(:show)
       expect(response).to have_http_status(:success)
-      @user.destroy
     end
   end
-
 end
--- spec/requests/saved_queries_spec.rb
+++ spec/requests/saved_queries_spec.rb
@@ -5,16 +5,20 @@ RSpec.describe "Saved Queries", type: :request do
     @user = User.create(email: 'UserEmail@email.com', first_name: 'Firstname', last_name: 'Lastname', username: 'user123', password: '1234567', db_activity: nil, last_db_activity: nil, admin: false)
     @user.confirm
     sign_in(@user)
-    # @user2 = User.create(email: 'UserEmail2@email.com', first_name: 'Firstname', last_name: 'Lastname2', username: 'user12345', password: '1234567', db_activity: nil, last_db_activity: nil, admin: false)
-    # @user2.confirm
+    @user2 = User.create(email: 'User2Email@email.com', first_name: 'Firstname', last_name: 'Lastname2', username: 'user12345', password: '1234567', db_activity: nil, last_db_activity: nil, admin: false)
+    @user2.confirm
   end
 
   after do
-    # delete all queries associated with user before deleting the user
+    # delete all Queries associated with each User before deleting each User
     @user.saved_queries.each do |query|
       query.destroy
     end  
     @user.destroy
+    @user2.saved_queries.each do |query|
+      query.destroy
+    end  
+    @user2.destroy
   end  
 
   describe "GET /saved_queries" do
@@ -28,7 +32,7 @@ RSpec.describe "Saved Queries", type: :request do
   describe "GET /saved_queries/:id" do
     it "renders the Saved Query show page" do
       saved_q = FactoryBot.create(:saved_query, user_id: @user.id)
-      get saved_query_path(saved_q.id)
+      get saved_query_path(id: saved_q.id)
       expect(response).to render_template(:show)
     end
     it "redirects to the Saved Queries index page if the Saved Query ID is invalid" do
@@ -52,12 +56,12 @@ RSpec.describe "Saved Queries", type: :request do
     end
   end
   
-  # describe "POST /saved_queries/ with invalid data" do
-  #   it "does not save a new Saved Query and renders the new page if invalid attribute (blank title)" do
-  #     saved_q = FactoryBot.attributes_for(:saved_query, title: '')
-  #     expect { post saved_queries_path, saved_query: saved_q }.to_not change(SavedQuery, :count)
-  #     expect(response).to render_template(:new)
-  #   end
+  describe "POST /saved_queries/ with invalid data" do
+    it "does not save a new Saved Query and renders the new page if invalid attribute (blank title)" do
+      saved_q = FactoryBot.attributes_for(:saved_query, title: '')
+      expect { post saved_queries_path, saved_query: saved_q }.to_not change(SavedQuery, :count)
+      expect(response).to render_template(:new)
+    end
   #   it "does not save a new Saved Query and renders the new page if invalid attribute (blank description)" do
   #     saved_q = FactoryBot.attributes_for(:saved_query, description: '')
   #     expect { post saved_queries_path, saved_query: saved_q }.to_not change(SavedQuery, :count)
@@ -68,56 +72,65 @@ RSpec.describe "Saved Queries", type: :request do
   #     expect { post saved_queries_path, saved_query: saved_q }.to_not change(SavedQuery, :count)
   #     expect(response).to render_template(:new)
   #   end
-  # end
+  end
 
   describe "GET /saved_queries/:id/edit" do
-    it "renders the Saved Query edit page" do
+    it "renders the Saved Query edit page if the current logged-in User is the User that created the Query" do
       saved_q = FactoryBot.create(:saved_query, user_id: @user.id)
-      get edit_saved_query_path(saved_q.id)
+      get edit_saved_query_path(id: saved_q.id)
       expect(response).to render_template(:edit)
+    end
+    it "renders the Saved Query edit page if the current logged-in User is NOT the User that created the Query" do
+      saved_q = FactoryBot.create(:saved_query, user_id: @user2.id)
+      get edit_saved_query_path(id: saved_q.id)
+      expect(response).to redirect_to(saved_queries_path)
     end
   end  
 
   describe "PUT /saved_queries/:id with valid data" do
     it "updates a Saved Query and redirects to the show page if valid attribute" do
       saved_q = FactoryBot.create(:saved_query, user_id: @user.id)
-      put saved_query_path(saved_q.id), saved_query: {title: 'Testing valid data.'}
+      put saved_query_path(id: saved_q.id), saved_query: {title: 'Testing valid data.'}
       saved_q.reload
       expect(saved_q.title).to eq('Testing valid data.')
       expect(response).to redirect_to saved_query_path
     end
   end
 
-  # describe "PUT /saved_queries/:id with invalid data" do
-  #   it "does not update a Saved Query and renders the edit page if invalid attribute (blank title)" do
-  #     saved_q = FactoryBot.create(:saved_query, user_id: @user.id)
-  #     put saved_query_path(saved_q.id), saved_query: {title: ''}
-  #     saved_q.reload
-  #     expect(saved_q.title).to_not eq('')
-  #     expect(response).to render_template(:edit)
-  #   end
+  describe "PUT /saved_queries/:id with invalid data" do
+    it "does not update a Saved Query and renders the edit page if invalid attribute (blank title)" do
+      saved_q = FactoryBot.create(:saved_query, user_id: @user.id)
+      put saved_query_path(id: saved_q.id), saved_query: {title: ''}
+      saved_q.reload
+      expect(saved_q.title).to_not eq('')
+      expect(response).to render_template(:edit)
+    end
   #   it "does not update a Saved Query and renders the edit page if invalid attribute (blank description)" do
   #     saved_q = FactoryBot.create(:saved_query, user_id: @user.id)
-  #     put saved_query_path(saved_q.id), saved_query: {description: ''}
+  #     put saved_query_path(id: saved_q.id), saved_query: {description: ''}
   #     saved_q.reload
   #     expect(saved_q.description).to_not eq('')
   #     expect(response).to render_template(:edit)
   #   end
   #   it "does not update a Saved Query and renders the edit page if invalid attribute (blank sql)" do
   #     saved_q = FactoryBot.create(:saved_query, user_id: @user.id)
-  #     put saved_query_path(saved_q.id), saved_query: {sql: ''}
+  #     put saved_query_path(id:saved_q.id), saved_query: {sql: ''}
   #     saved_q.reload
   #     expect(saved_q.sql).to_not eq('')
   #     expect(response).to render_template(:edit)
   #   end  
-  # end
+  end
 
   describe "DELETE /saved_queries/:id " do
-    it "destroys a Saved Query and redirects to the index page" do
+    it "destroys a Saved Query and redirects to the index page if the current logged-in User is the User that created the Query" do
       saved_q = FactoryBot.create(:saved_query, user_id: @user.id)
-      expect { delete saved_query_path(saved_q.id) }.to change(SavedQuery, :count)
+      expect { delete saved_query_path(id: saved_q.id) }.to change(SavedQuery, :count)
       expect(response).to redirect_to saved_queries_path
     end
-  end  
-    
+    it "does NOT destroy a Saved Query and renders the show page if the current logged-in User is NOT the User that created the Query" do
+      saved_q = FactoryBot.create(:saved_query, user_id: @user2.id)
+      expect { delete saved_query_path(id: saved_q.id) }.to_not change(SavedQuery, :count)
+      expect(response).to render_template(:show)
+    end
+  end      
 end    
