b2433e71ee06bc5de7f4d32e95c969745c3f2041
Ievgen Dotsenko <66342665+ievgend2@users.noreply.github.com>
2023-02-15 09:50:22 -0500

    Created FileDownload Model

--- db/migrate/20230131123222_create_file_downloads.rb
+++ db/migrate/20230131123222_create_file_downloads.rb
@@ -2,7 +2,6 @@ class CreateFileDownloads < ActiveRecord::Migration
   def change
     create_table :file_downloads do |t|
       t.integer :file_record_id     
-      t.integer :count, :default => 0
       t.timestamps
     end
   end
