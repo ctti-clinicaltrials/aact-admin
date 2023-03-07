db0e80bed69c39a940d184b1a3feaffa378487c9
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-03-07 11:43:30 -0800

    Created destroy action for BackgroundJobsController. Added Delete button to the show page.

--- app/controllers/background_jobs_controller.rb
+++ app/controllers/background_jobs_controller.rb
@@ -1,4 +1,5 @@
 class BackgroundJobsController < ApplicationController
+  before_action :set_background_job, only: [:destroy]
 
   def index
     @background_jobs = Core::BackgroundJob.all.order(created_at: :desc)
@@ -12,5 +13,15 @@ class BackgroundJobsController < ApplicationController
   end
 
   def destroy
+    @background_job.destroy
+    redirect_to background_jobs_path, notice: "The background job was deleted successfully."
   end
+
+  private
+    def set_background_job
+      @background_job = Core::BackgroundJob.find_by_id(params[:id])
+      if @background_job.nil?
+        render :file => "app/views/errors/not_found.html", status: :not_found
+      end  
+    end
 end
