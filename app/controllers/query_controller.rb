e7a65afacf3977052d96e37270fae0dff319593a
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-03-27 12:30:40 -0700

    Modified QueryController index action. Modified the current query submission system so that instead of running the job, we create a background job. Removed formatting CSV file.

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,31 +1,17 @@
 class QueryController < ApplicationController
-  require 'csv'
 
   def index
     if params[:query]
       begin
-        @results = Query::Base.connection.execute(params[:query])
-      # if there is an error in the SQL query, display the form again with the error message
-      rescue ActiveRecord::StatementInvalid => e
-        @error = [e.message]
-      end
-    end
-
-    respond_to do |format|
-      format.csv  do
-        response.headers['Content-Type'] = 'text/csv'
-        response.headers['Content-Disposition'] = 'attachment; filename=query.csv'
-        # get headers from the keys of query results to generate first line of csv file
-        headers = @results.first.map { |key,value| key }
-        csv = CSV.generate_line headers
-        # get rows from the values of the keys of query results to generate rest of lines of csv file
-        @results.each do |result|
-          rows = result.each.map { |key,value| value }  
-          csv << CSV.generate_line(rows).html_safe
+        # instead of running the query, create a background job if user has less than 10 jobs in the queue    
+        if current_user.background_jobs.where(status: ['pending', 'running']).count < 10
+          @background_job = BackgroundJob.create(user_id: current_user.id, status: 'pending', type: 'BackgroundJob', data: params[:query])
+          redirect_to background_job_path(@background_job.id), notice: "The query was added to the queue successfully."
+        else 
+          redirect_to background_jobs_path, alert: "Cannot run query. You can only have a maximum of 10 queries in the queue."
         end
-        render plain: csv
-      end
-      format.html { render template: 'query/index.html.erb' }
-    end
+      end    
+    end  
   end
+
 end
