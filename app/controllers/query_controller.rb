df865439b6b715b6b5e36e468f78294c77d058aa
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-04-01 11:26:33 -0700

    Ceated model class BackgroundJob::Query < BackgroundJob. Modified the Query submission so that the type is now BackgroundJob::Query in the QueryController index action. Removed SQL Query Results and Export CSV from the Query index page.

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -5,7 +5,7 @@ class QueryController < ApplicationController
       begin
         # instead of running the query, create a background job if user has less than 10 jobs in the queue    
         if current_user.background_jobs.where(status: ['pending', 'running']).count < 10
-          @background_job = BackgroundJob.create(user_id: current_user.id, status: 'pending', type: 'BackgroundJob', data: params[:query])
+          @background_job = BackgroundJob::Query.create(user_id: current_user.id, status: 'pending', type: 'BackgroundJob', data: params[:query])
           redirect_to background_job_path(@background_job.id), notice: "The query was added to the queue successfully."
         else 
           redirect_to background_jobs_path, alert: "Cannot run query. You can only have a maximum of 10 queries in the queue."
