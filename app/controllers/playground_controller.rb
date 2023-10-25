9309f6a6fd6d3d981d3b661c828ebf473c492019
kostik <kostik@aact.myguest.virtualbox.org>
2023-10-25 13:45:41 -0400

    file name fixed

--- app/controllers/playground_controller.rb
+++ app/controllers/playground_controller.rb
@@ -1,6 +1,7 @@
 class PlaygroundController < ApplicationController
 
   def index
+    byebug
     if params[:query]
       # instead of running the query, create a background job if user has less than 10 jobs in the queue    
       if current_user.background_jobs.where(status: ['pending', 'running']).count < 10
