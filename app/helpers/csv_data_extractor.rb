fe2224710c2713f19d777662e1adc057f438dac5
kostik <kostik@aact.myguest.virtualbox.org>
2023-10-30 12:38:31 -0400

    number of columns limited by 10, CSS updated

--- app/helpers/csv_data_extractor.rb
+++ app/helpers/csv_data_extractor.rb
@@ -4,6 +4,7 @@ require 'httparty'
 class CsvDataExtractor
   def initialize(api_url)
     @api_url = api_url
+    @max_columns = 10
   end
 
   def fetch_and_extract_data
@@ -12,7 +13,9 @@ class CsvDataExtractor
     if response.success?
       csv_data = response.body
       headers, data = parse_csv(csv_data)
-      return headers, data[0...100] # Extract the first 100 rows
+      headers = headers[0...@max_columns]  # Limit the headers to the first 10 columns
+      data = data.map { |row| row[0...@max_columns] }  # Limit the data to the first 10 columns
+      return headers, data[0...100]  # Extract the first 100 rows
     else
       raise "Failed to fetch CSV data from the API: #{response.code}"
     end
