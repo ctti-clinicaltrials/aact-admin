ca6d8706035a4f08db4fa6051d05cfd42a2cafac
Kassidy <kassidy.holtzman@outlook.com>
2023-12-12 15:00:01 -0500

    Made changes - only want to render not found if file_record is nil

--- spec/controllers/file_records_controller_spec.rb
+++ spec/controllers/file_records_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe FileRecordsController, type: :controller do
   describe "GET #active_url" do
     context "when file record is found" do
-      it "returns http success (204)" do
+      it "returns http success (302)" do
         create(:file_record, created_at: '2023-03-27', url: 'https://www.example.com')
         get :active_url, params: { type: 'static_db_copies', time: 'daily', filename: '2023-03-27' }, format: :json
-        expect(response).to have_http_status(204)
+        expect(response).to have_http_status(302)
       end
     end
 
