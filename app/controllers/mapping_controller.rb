43d782960c5f516c013919c56a7cf8a761a2aaf3
akabishau <aleksey.kabishau@icloud.com>
2024-10-21 22:50:15 -0400

    add mapping response from cache

--- app/controllers/mapping_controller.rb
+++ app/controllers/mapping_controller.rb
@@ -1,7 +1,7 @@
 class MappingController < ApplicationController
 
   def index
-    @mappings = fetch_data
+    @mappings = fetch_and_cache_data
 
     if params[:filter].present?
       @mappings = filter_data(@mappings, params[:filter])
@@ -10,8 +10,16 @@ class MappingController < ApplicationController
 
   private
 
+  # check if data is cached, if not, fetch from API
+  def fetch_and_cache_data
+    Rails.cache.fetch("mapping_data", expires_in: 4.hours) do
+      fetch_data_from_api
+    end
+  end
 
-  def fetch_data
+
+  def fetch_data_from_api
+    Rails.logger.info "Fetching mapping data from API"
     url = "http://localhost:3000/api/v1/mapping"
     response = HTTParty.get(url)
     response.parsed_response # httparty parses based on content type
