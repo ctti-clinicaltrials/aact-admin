2e14145a79e567106b07d10915fc1fdb330a7c35
akabishau <aleksey.kabishau@icloud.com>
2024-12-07 20:31:08 -0500

    move csv logic to the docs service

--- app/controllers/documentation_controller.rb
+++ app/controllers/documentation_controller.rb
@@ -9,17 +9,9 @@ class DocumentationController < ApplicationController
   def index
     @docs = @docs_service.fetch_and_cache_data
     @docs = filter_data(@docs)
-
-    respond_to do |format|
-      format.html do
-        @paginated_docs = Kaminari.paginate_array(@docs).page(params[:page]).per(20)
-      end
-      format.csv do
-        csv_data = fetch_csv_from_api
-        send_data csv_data[:body], filename: "documentation_#{Time.now.strftime("%Y%m%d")}.csv", type: "text/csv"
-      end
-    end
+    @paginated_docs = Kaminari.paginate_array(@docs).page(params[:page]).per(20)
   end
+
 
   def show
   end
@@ -32,7 +24,6 @@ class DocumentationController < ApplicationController
     end
   end
 
-
   def update
     if @docs_service.update_doc_item(@doc_item, doc_params)
       redirect_to documentation_path(@doc_item["id"]), notice: "Item Updated"
@@ -40,6 +31,11 @@ class DocumentationController < ApplicationController
       flash.now[:alert] = "Something went wrong. Please try again."
       render :edit
     end
+  end
+
+  def download_csv
+    csv_data = @docs_service.fetch_csv_from_api
+    send_data csv_data[:body], filename: "documentation_#{Time.now.strftime("%Y%m%d")}.csv", type: "text/csv"
   end
 
   # TODO: Only save in case if response is success
@@ -59,53 +55,6 @@ class DocumentationController < ApplicationController
   def set_doc_item
     @doc_item = @docs_service.fetch_and_cache_data.find { |doc| doc["id"] == params[:id].to_i }
     redirect_to documentation_index_path, alert: "Something went wrong" unless @doc_item
-  end
-
-  def fetch_documentation_format(format)
-    url = ENV["DOCUMENTATION_API_URL"]
-    if format == "csv"
-      response = HTTParty.get(url, headers: { "Accept" => "text/csv" })
-    elif format == "html"
-      response = HTTParty.get(url)
-    else
-      puts "Invalid format"
-    end
-  end
-
-  def handle_csv_response(response)
-  end
-
-  def handle_html_response(response)
-
-  end
-
-  def fetch_csv_from_api
-    Rails.logger.info "Fetching CSV documentation from Cache"
-    Rails.cache.fetch("csv_mapping_data", expires_in: 10.minutes) do
-      Rails.logger.info "Cache is not available -> Fetching CSV documentation from API"
-      url = ENV["DOCUMENTATION_API_URL"]
-      response = HTTParty.get(url, headers: { "Accept" => "text/csv" })
-
-      if response.success?
-        { body: response.body }
-      else
-        Rails.logger.error "Failed to fetch CSV data: #{response.message}"
-        { body: "" }
-      end
-    rescue StandardError => e
-      Rails.logger.error "Failed to fetch CSV data: #{e.message}"
-      { body: "" }
-    end
-  end
-
-  def fetch_data_from_api
-    Rails.logger.info "Cache is not available -> Fetching documentation from API"
-    url = ENV["DOCUMENTATION_API_URL_LOCAL"]
-    response = HTTParty.get(url)
-    response.parsed_response
-  rescue StandardError => e
-    Rails.logger.error "Failed to fetch data: #{e.message}"
-    []
   end
 
   def filter_data(mappings)
--- app/services/documentation_service.rb
+++ app/services/documentation_service.rb
@@ -1,12 +1,32 @@
 # app/services/documentation_service.rb
 class DocumentationService
-  CACHE_KEY = "docs_data"
+  CACHE_KEY = "docs_json"
+  CSV_CACHE_KEY = "docs_csv"
   API_URL = ENV["DOCUMENTATION_API_URL_LOCAL"]
 
   def fetch_and_cache_data
     Rails.logger.info "Fetching documentation from Cache"
     Rails.cache.fetch(CACHE_KEY, expires_in: 10.minutes) do
       fetch_data_from_api
+    end
+  end
+
+  def fetch_csv_from_api
+    Rails.logger.info "Fetching CSV documentation from Cache"
+    Rails.cache.fetch(CSV_CACHE_KEY, expires_in: 10.minutes) do
+      Rails.logger.info "Cache is not available -> Fetching CSV documentation from API"
+      url = ENV["DOCUMENTATION_API_URL"]
+      response = HTTParty.get(url, headers: { "Accept" => "text/csv" })
+
+      if response.success?
+        { body: response.body }
+      else
+        Rails.logger.error "Failed to fetch CSV data: #{response.message}"
+        { body: "" }
+      end
+    rescue StandardError => e
+      Rails.logger.error "Failed to fetch CSV data: #{e.message}"
+      { body: "" }
     end
   end
 
@@ -22,7 +42,18 @@ class DocumentationService
 
   private
 
+   # def fetch_data_from_api
+  #   Rails.logger.info "Cache is not available -> Fetching documentation from API"
+  #   url = ENV["DOCUMENTATION_API_URL_LOCAL"]
+  #   response = HTTParty.get(url)
+  #   response.parsed_response
+  # rescue StandardError => e
+  #   Rails.logger.error "Failed to fetch data: #{e.message}"
+  #   []
+  # end
+
   def fetch_data_from_api
+    "No Cache -> Fetching documentation from API"
     response = HTTParty.get(API_URL)
     JSON.parse(response.body)
   end
@@ -42,5 +73,6 @@ class DocumentationService
 
   def invalidate_cache
     Rails.cache.delete(CACHE_KEY)
+    Rails.cache.delete(CSV_CACHE_KEY)
   end
 end
