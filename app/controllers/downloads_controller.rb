88c56114f09737033625688598dd70d07e922173
akabishau <aleksey.kabishau@icloud.com>
2025-04-14 09:55:18 -0400

    add empty state for latest snapshots

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,14 +2,7 @@ class DownloadsController < ApplicationController
   before_action :set_snapshots_service
 
   def index
-    @latest_snapshots = @snapshots_service.fetch_latest_snapshots
-
-    if @latest_snapshots.nil?
-      flash[:alert] = "Failed to fetch latest snapshots."
-      @latest_snapshots = []  # Use an empty array instead of an empty hash
-    end
-
-    Rails.logger.info "Latest snapshots data: #{@latest_snapshots.inspect}"
+    @latest_snapshots = @snapshots_service.fetch_latest_snapshots # [] as fallback from service
   end
 
   def snapshots
